702453835fbb5a766d73308f2fb4622c317794f8
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-07-28 02:33:33 -0300

    dia 01 - imersao react alura

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"Uma biblioteca JavaScript para criar interfaces de usuário, declarativo, baseado em componentes, fazer interfaces com outras bibliotecas e estruturas!"}
       />
 
       <Carousel
